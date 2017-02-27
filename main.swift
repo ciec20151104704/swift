84ee942488430dade324190a79cd245053dc4bcd
ciec20151104704 <497797188@qq.com>
2017-02-27 11:52:03 +0800

    01

---
+++ main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  1+...+100
+//
+//  Created by s20151104704 on 17/2/27.
+//  Copyright © 2017年 s20151104704. All rights reserved.
+//
+
+import Foundation
+
+var sum=0
+for i in 0..<101{
+    sum = sum+i
+}
+print(sum)
+
